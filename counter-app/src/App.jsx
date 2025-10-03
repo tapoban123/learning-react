d1b9a0330e6aa12f61c7d677b29cbcf7a1c993a1
Tapoban Ray <raytapoban@gmail.com>
2025-10-03 21:11:38 +0530

    Learnt to useState and built CounterApp

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [counter, setCounter] = useState(15);
+
+  const addCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const subtractCounter = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+
+  return (
+    <>
+      <h1>My Counter App</h1>
+      <br />
+      <h2>Counter Value: {counter}</h2>
+      <br />
+
+      <button onClick={addCounter}>Increase {counter}</button>
+      <br />
+      <br />
+      <button onClick={subtractCounter}>Decrease {counter}</button>
+    </>
+  );
+}
+
+export default App;
