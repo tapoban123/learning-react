64eeaf175db491c140e2aca8d6d92405ebf5369f
Tapoban Ray <raytapoban@gmail.com>
2025-10-03 18:33:41 +0530

    Learnt most basic foundational concepts of React

--- ReactViteBasics/src/App.jsx
+++ ReactViteBasics/src/App.jsx
@@ -1,6 +1,12 @@
+import Hello from "./Hello";
 
 function App() {
-  return (<h1>Hello World from Tapoban Ray</h1>);
+  return (
+    <>
+      <h1>Hello World from Tapoban Ray</h1>
+      <Hello></Hello>
+    </>
+  );
 }
 
 export default App;
---
+++ ReactViteBasics/src/Hello.jsx
@@ -0,0 +1,7 @@
+function Hello(){
+    return (
+        <h2>I am learning React.</h2>
+    )
+}
+
+export default Hello
